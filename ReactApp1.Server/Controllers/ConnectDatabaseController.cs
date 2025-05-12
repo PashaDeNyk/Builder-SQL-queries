d362396d3b9c20713fea8572cbfdb73827959108
PashaDenyk <pavel.danukov@gmail.com>
2025-05-12 19:00:51 +0300

    Добавлен отдельный контроллер для подключения к базе данных

---
+++ ReactApp1.Server/Controllers/ConnectDatabaseController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using ReactApp1.Server.Models;
+
+namespace ReactApp1.Server.Controllers
+{
+    [ApiController]
+    [Route("api/database")]
+    public class ConnectDatabaseController : Controller
+    {
+        [HttpPost("---")]
+        public IActionResult Connect([FromBody] DBConfig config)
+        {
+            try
+            {
+                var connectionString = $"Host={config.Host};Port={config.Port};Database={config.Database};Username={config.Username};Password={config.Password}";
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+                    connection.Close();
+                }
+                    return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { Error = e.Message });
+            }
+        }
+    }
+}
