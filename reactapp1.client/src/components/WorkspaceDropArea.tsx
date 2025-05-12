fbba41cd792ff25feb0c348f7d4f451801a30649
MattKenyei <mattkenyei@gmail.com>
2025-05-12 14:51:40 +0300

    add new func, replace loading tables to connect page, add tables store

--- reactapp1.client/src/components/WorkspaceDropArea.tsx
+++ reactapp1.client/src/components/WorkspaceDropArea.tsx
@@ -16,11 +16,9 @@ export const WorkspaceDropArea: React.FC<WorkspaceDropAreaProps> = ({
         drop: (item: { tables: Table[] }, monitor) => {
             const offset = monitor.getSourceClientOffset();
 
-            // Обрабатываем как массив таблиц
             if (item.tables && Array.isArray(item.tables)) {
                 onDropItem(item.tables, offset);
             }
-            // Для обратной совместимости с одиночными таблицами
             else if (item.table) {
                 onDropItem([item.table], offset);
             }
