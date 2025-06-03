5be7728151e5b347cf3468545a13cbab1c6268a6
PashaDenyk <pavel.danukov@gmail.com>
2025-06-03 22:50:33 +0300

    +

--- ReactApp1.Server/Controllers/CreateSQLController.cs
+++ ReactApp1.Server/Controllers/CreateSQLController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ReactApp1.Server.Models;
+using System.Globalization;
 
 namespace ReactApp1.Server.Controllers
 {
@@ -8,11 +9,51 @@ namespace ReactApp1.Server.Controllers
     public class CreateSQLController : Controller
     {
         [HttpPost]
-        public IActionResult CreateSQL([FromBody] QueryModel queryModel)
+        public IActionResult CreateSQL([FromBody] QueryModel queryModel)//ОСНОВНАЯ ПРОБЛЕМА С ИСПОЛЬЗОВАНИЕМ LIST
         {
             string SQL = "";
 
-            return Ok();
+            if(queryModel.Select == null) //без SELECT ничего не получится
+            {
+                return BadRequest("Wrong query model -> need SELECT");
+            }
+
+
+            if (queryModel.Select != null)//внутри селекта могут быть AVG COUNT и также их название по типу COUNT(*) as count_c
+            {
+                SQL += $"Select {queryModel.Select} from {queryModel.Name}";
+            }
+
+            //временно, пока не увверен в item3, item4 -> это если не cross join и нужно условие
+            if (queryModel.Join != null && queryModel.Join.Item3 == null && queryModel.Join.Item4 == null)
+            {
+                //item1 - тип (cross, inner...)   item2 - название таблицы
+                SQL += $"{queryModel.Join.Item1} {queryModel.Join.Item2}";//это если cross join, в других есть условие
+            }
+            else if (queryModel.Join.Item3 != null && queryModel.Join.Item4 != null)
+            {
+                //item1 - тип (cross, inner...)   item2 - название таблицы   item3 - названия столбца от которого тянут связь   item4 - название столбца к которому тянут свзяь
+                SQL += $"{queryModel.Join.Item1} {queryModel.Join.Item2} on {queryModel.Join.Item3} = {queryModel.Join.Item4}";
+            }
+            else { return BadRequest("Wrong query model -> wrong Join"); }//если есть item3 но нет item4 и наоборот 
+
+            //что если несколько условий
+            if (queryModel.Where != null)
+            {
+                SQL += $"Where {queryModel.Where.Item1} {queryModel.Where.Item2} {queryModel.Where.Item3}";
+            }
+
+            if (queryModel.GroupBy!=null)
+            {
+
+            }
+
+            if (queryModel.Having!=null)
+            {
+
+            }
+            SQL += ";";//необязательно
+            return Ok(SQL);
         }
     }
 }
--- ReactApp1.Server/Models/QueryModel.cs
+++ ReactApp1.Server/Models/QueryModel.cs
@@ -1,17 +1,41 @@
 ﻿namespace ReactApp1.Server.Models
 {
-
+    //переменные могут быть пустыми
+    //действительно ли нужны List?
+    //Если несколько значений в одной строке, то это всё ещё одна строка или несколько маленьких?
     public class QueryModel
     {
-        public string Name { get; set; } 
-        public string Select { get; set; }
+        public string Name { get; set; } //имя таблицы для FROM
+
+
+        public string Select { get; set; } //названия столбцов для select
+        //обработать использование пользовательских названий переменных и агрегатные функции по типу sum avg count
+        
+
+        public Tuple<string, string, string,string> Join { get; set; } // тип и название таблицы, если не cross join, то и названия столбцов, также там всегда =, так как id>id это глупость
+                                                                       // обработать момент с тем чтобы давать названия таблица по типу "product p on p.product_id..."
+
         //public TwoStrOneOperator Where { get; set; }
-        public List<Tuple<string, string, string>> Where { get; set; }
-        public string GroupBy { get; set; }
+
+        //!!!!!!!!!!!!!!!!!!!!
+        //Возможно поэтому нужен List, так что если в нём более одного элемента ставить and или or, но это может выбирать пользователь
+        //!!!!!!!!!!!!!!!!!!!!
+        public Tuple<string, string, string> Where { get; set; } //str1 str2 operator
+                                                                 //WHERE department = 'IT' AND salary > 5000;
+                                                                 //также проработать работу с датами
+
+        public string GroupBy { get; set; }//возможно по столбцам сортировать например "GROUP BY 1"
+        
+        
         //public TwoStrOneOperator Having {get;set;}
-        public List<Tuple<string, string, string>> Having { get; set; }
+        public Tuple<string, string, string> Having { get; set; } // фильтрация после группировки
+                                                                  // str1 str2 operator
+                                                                  // можно считать условием группировки
+        
+        
         //public TwoStrOneOperator OrederBy {get;set;}
-        public List<Tuple<string,string,string>> OrederBy {get;set;}
+        public Tuple<string,string,string> OrederBy {get;set;}
+
 
     }
 
